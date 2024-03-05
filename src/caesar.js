a9e326543c243dfc1d1f4e7c47cc78dfd87e0459
marcel williams <158129295+MarcelDWill@users.noreply.github.com>
2024-03-05 14:21:51 -0500

    Update caesar.js

--- src/caesar.js
+++ src/caesar.js
@@ -2,7 +2,7 @@
 // as the index.html and test files rely on this setup to work properly.
 // Only add code (helper methods, variables, etc.) within the scope
 // of the anonymous function on line 6
-
+ 
 const caesarModule = (function () {
   // you can add any code you want within this function scope
 
